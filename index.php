5ffc18717c94f3cddd921199ce5ae99ab40bfd3b
bsrubiog <89091844+bsrubiog@users.noreply.github.com>
2025-10-06 17:29:02 -0500

    Update index.php
    BR

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <?php 
 // Connection variables 
+// cambio realizado por Brandon Rubio 
 $host = "localhost"; // MySQL host name eg. localhost
 $user = "root"; // MySQL user. eg. root ( if your on localserver)
 $password = ""; // MySQL user password  (if password is not set for your root user then keep it empty )
